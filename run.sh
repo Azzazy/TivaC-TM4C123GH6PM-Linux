2724dd6b4c077353792aa8a2fd8d6b181041741a
muhammad <msa.azzazy@aucegypt.edu>
2018-05-04 12:26:58 +0200

    New chaintool and path fix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export PATH=$PATH:$HOME/Documents/Courses/ARM/workplace/gcc-arm-none-eabi-5_4-2016q3/bin
+export PATH=$PATH:$HOME/Documents/Courses/ARM/workplace/gcc-arm-none-eabi-7-2017-q4-major/bin
 cd ~/Documents/Courses/ARM/workplace/tiva-template/
 sudo rm -r build/
 make
 cd ~/Documents/Courses/ARM/workplace/lm4tools/lm4flash/
-sudo ./lm4flash ~/Documents/Courses/ARM/workplace/tiva-template/build/main.bin
+sudo ./lm4flash ~/Documents/Courses/ARM/workplace/build/main.bin
--- src/startup_gcc.c
+++ src/startup_gcc.c
@@ -44,7 +44,6 @@ static void IntDefaultHandler(void);
 //*****************************************************************************
 extern int main(void);
 
-extern void Timer0IntHandler(void);
 //*****************************************************************************
 //
 // Reserve space for the system stack.
@@ -97,7 +96,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // ADC Sequence 2
     IntDefaultHandler,                      // ADC Sequence 3
     IntDefaultHandler,                      // Watchdog timer
-    Timer0IntHandler,                      // Timer 0 subtimer A
+    IntDefaultHandler,                      // Timer 0 subtimer A
     IntDefaultHandler,                      // Timer 0 subtimer B
     IntDefaultHandler,                      // Timer 1 subtimer A
     IntDefaultHandler,                      // Timer 1 subtimer B
