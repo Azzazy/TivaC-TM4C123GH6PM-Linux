7895e252a97ff31532de5fe272caf60bee6d3008
muhammad <msa.azzazy@aucegypt.edu>
2018-05-04 12:43:06 +0200

    Directory organization

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:$HOME/Documents/Courses/ARM/workplace/gcc-arm-none-eabi-7-2017-q4-major/bin
-cd ~/Documents/Courses/ARM/workplace/tiva-template/
+cd ~/Documents/Courses/ARM/workplace/
 sudo rm -r build/
 make
-cd ~/Documents/Courses/ARM/workplace/lm4tools/lm4flash/
+cd ~/Documents/Courses/ARM/workplace/lm4flash/
 sudo ./lm4flash ~/Documents/Courses/ARM/workplace/build/main.bin
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "inc/tm4c123gh6pm.h"
 #include "inc/hw_types.h"
 #include "inc/hw_gpio.h"
 #include "inc/hw_memmap.h"
@@ -10,27 +11,67 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/can.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/uart.h"
 
-#define LED_RED GPIO_PIN_1
-#define LED_BLUE GPIO_PIN_2
-#define LED_GREEN GPIO_PIN_3
-
-int main()
+void UARTIntHandler(void)
 {
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+	uint32_t ui32Status;
+	ui32Status = ROM_UARTIntStatus(UART0_BASE, true); //get interrupt status
+	ROM_UARTIntClear(UART0_BASE, ui32Status); //clear the asserted interrupts
+	while(ROM_UARTCharsAvail(UART0_BASE)) //loop while there are chars
+	{
+		char c = ROM_UARTCharGetNonBlocking(UART0_BASE);
+		//ROM_UARTCharPutNonBlocking(UART0_BASE, c); //echo character
+		if (c=='y'){
+			ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //turn led on
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'L');		
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'E');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'D');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, ' ');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'O');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'N');
+			ROM_UARTCharPutNonBlocking(UART0_BASE, '\n'); 			
+		}else if (c=='n'){
+			ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); //turn off LEDf	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'L');		
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'E');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'D');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, ' ');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'O');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'F');	
+			ROM_UARTCharPutNonBlocking(UART0_BASE, 'F');
+			ROM_UARTCharPutNonBlocking(UART0_BASE, '\n'); 	
+		}
+		//ROM_SysCtlDelay(ROM_SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
+	}
+}
 
-   for (;;) {
-	// set the red LED pin high, others low
-	ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_BLUE);
-	ROM_SysCtlDelay(5000000);
-	ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_GREEN);
-	ROM_SysCtlDelay(5000000);
-	ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_BLUE);
-	ROM_SysCtlDelay(5000000);
-	ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-	ROM_SysCtlDelay(10000000);
-    }
-
+int main(void) {
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+	ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+	ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //enable GPIO port for LED
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2); //enable pin for LED PF2
+	ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+	(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+	ROM_IntMasterEnable(); //enable processor interrupts
+	ROM_IntEnable(INT_UART0); //enable the UART interrupt
+	ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT); //only enable RX and TX interrupts
+	ROM_UARTCharPut(UART0_BASE, 'S');
+	ROM_UARTCharPut(UART0_BASE, 't');
+	ROM_UARTCharPut(UART0_BASE, 'a');
+	ROM_UARTCharPut(UART0_BASE, 'r');
+	ROM_UARTCharPut(UART0_BASE, 't');
+	ROM_UARTCharPut(UART0_BASE, 'e');
+	ROM_UARTCharPut(UART0_BASE, 'd');
+	ROM_UARTCharPut(UART0_BASE, '!');
+	ROM_UARTCharPut(UART0_BASE, '\n');
+	while (1) //let interrupt handler do the UART echo function
+	{
+		//if (ROM_UARTCharsAvail(UART0_BASE)) ROM_UARTCharPut(UART0_BASE, ROM_UARTCharGet(UART0_BASE));
+	}
 }
